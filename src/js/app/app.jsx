20b76ad85bf227fe2cd52c1db10bae6a114fa991
Ty Tia <TyTia77@gmail.com>
2018-03-31 14:40:24 +1100

    code cleanup

--- src/js/app/app.jsx
+++ src/js/app/app.jsx
@@ -1,5 +1,7 @@
-import React from "react"
+import React from 'react'
 import propTypes from 'prop-types'
+import { connect } from 'react-redux'
+import { fetchWeather } from '../actions/weather-action'
 
 import Subs from './containers/subs/subs'
 import Drinks from './containers/drinks/drinks'
@@ -14,19 +16,30 @@ import SidesAM from './containers/sides-am/sides-am'
 require('StylePath/reset.scss')
 require('./app.scss')
 
+// connects redux with react
+@connect(store => {
+    return {
+        weather: store.weather.weather
+    }
+})
 export default class App extends React.Component {
 
     componentWillMount() {
-        
+
         let timeline = new SB.Timeline({
             debug: false, // truthy value, will visualise the timeline
             framerate: 30, // frames per second, default: 25
             cycleTime: 3000, // milliseconds, length of the timeline cycle, default: 0
-        });
+        })
         this.setState({
             items: this.props.data,
             timeline
         })
+
+
+        // dispatch action
+        // this.props.dispatch(fetchWeather())
+        console.log('props', this.props)
     }
 
     render() {
